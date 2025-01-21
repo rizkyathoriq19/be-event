860a99001d910b6f6abec9d2cd78acab04253108
rizkyathoriq19 <rizkyathoriq19@gmail.com>
2025-01-21 10:30:17 +0700

    feat: api auth me

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import * as Yup from 'yup';
 import UserModel from '../models/user.model';
 import { encrypt } from '../utils/encryption';
+import { generateToken } from '../utils/jwt';
+import { IReqUser } from '../middlewares/auth.middleware';
 
 type TRegister = {
     fullName: string;
@@ -85,10 +87,33 @@ export default {
                 })                
             }
 
+            const token = generateToken({
+                id: userByIdentifier._id,
+                role: userByIdentifier.role,
+            });
+
             res.status(200).json({
                 message: "Login success",
-                data: userByIdentifier
+                data: token
             });
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data: null
+            })
+        }
+    },
+
+    async me(req: IReqUser, res: Response) {
+        try {
+            const user = req.user;
+            const result = await UserModel.findById(user?.id);
+
+            res.status(200).json({
+                message: "Success get user profile",
+                data: result
+            })
         } catch (error) {
             const err = error as unknown as Error;
             res.status(400).json({
---
+++ src/utils/jwt.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken"
+import { Types } from "mongoose"
+import { User } from "../models/user.model"
+import { SECRET } from "./env";
+
+export interface IUserToken
+    extends Omit<
+        User,
+        | "password"
+        | "activationCode"
+        | "isActived"
+        | "email"
+        | "fullName"
+        | "profilePicture"
+        | "username"
+    > { 
+        id?: Types.ObjectId;
+    }
+
+export const generateToken = (user: IUserToken): string => { 
+    const token = jwt.sign(user, SECRET, {
+        expiresIn: "1h",
+    })
+    return token;
+}
+
+export const getUserData = (token: string) => { 
+    const user = jwt.verify(token, SECRET) as IUserToken;
+    return user;
+}
